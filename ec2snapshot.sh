b022f45e3712d6d21be35d73d1d85d1e613393e7
AWS ParallelCluster user <ec2-user@ip-10-0-5-98.ap-southeast-1.compute.internal>
2022-10-29 03:07:33 +0000

    Committing for merge

--- ec2snapshot.sh
+++ ec2snapshot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh -o StrictHostKeyChecking=no -i /data/MyKeyPair.pem ec2-user@18.141.213.214 "source ~/.bashrc; /home/ec2-user/.local/bin/pcluster update-compute-fleet --status STOP_REQUESTED --region ap-southeast-1 --cluster-name MyCluster01; ~/update_snapshot.sh data 2 MyCluster01"
+ssh -o StrictHostKeyChecking=no -i /data/MyKeyPair.pem ec2-user@18.141.213.214 "source ~/.bashrc; /home/ec2-user/.local/bin/pcluster update-compute-fleet --status STOP_REQUESTED --region ap-southeast-1 --cluster-name MyCluster02; ~/update_snapshot.sh data 2 MyCluster02"
